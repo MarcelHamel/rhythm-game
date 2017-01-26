7f0be0d3807e65e6ac7a2c3474366100b5b7ad91
Marcel Hamel <marcel.hamel@gmail.com>
2017-01-26 16:34:58 -0500

    Completed button event listener and playback functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+  // Storing query selections...
+  const playButton = document.querySelector('#play');
+  const stopButton = document.querySelector('#stop');
+  const submitButton = document.querySelector('#submit');
+  const inputField = document.querySelector('input');
+  const keys = document.querySelectorAll('.key');
+  const lights = document.querySelectorAll('.light');
+
   // Variables which check for game state and store user input
   var playbackSequence = [[1,2],[],[1,2],[2],[1],[2],[1,2],[2]];
-  var userSequence = [[],[],[],[],[],[],[],[]];
+  var userSequence = [[1],[],[1],[],[1],[],[1],[]];
 
   // Set tempo functionality
   var bpm = 312
   var newTempo = function(input) {
-    var interval = document.querySelector('input').value;
+    var interval = inputField.value;
     bpm = 60000 / (input * 2);
   }
 
@@ -15,42 +23,78 @@ document.addEventListener('DOMContentLoaded', function(){
   function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('lit');
-  }
-  // Animation event listeners... SEE README
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  };
 
-  // Playback functionality
+  // Animation event listeners... SEE README
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  lights.forEach(light => light.addEventListener('transitionend', removeTransition));
+
+  // Playback functionality...
+  // MDN suggests this to clear timeout intervals from other functions. Not sure why. ASK.
+  var intervalID;
+
+  // Actual playback function
   var playSequence = function(rhythm) {
     var seqPos = 0;
-      setInterval(function(){
+      intervalID = setInterval(function(){
         rhythm[seqPos].forEach(function(i){
-            var audio = document.querySelector(`audio.dataset.note='${i}'`);
+            var audio = document.querySelector(`audio[data-note='${i}']`);
             audio.currentTime = 0;
             audio.play();
           });
-        var light = document.querySelector(`.light #${seqPos}`);
+        var light = document.querySelector(`.light[data-index='${seqPos}']`);
         light.classList.add('lit');
         if (rhythm[seqPos].length > 0) {
-          document.querySelector(`.key.id = ${seqPos}`)
+          document.querySelector(`.key[data-index='${seqPos}']`)
                   .classList
                   .add('lit');
         }
+        seqPos < 7 ? seqPos++ : seqPos = 0;
       }, bpm);
   };
 
   //Sequence Key event listener
-  document.querySelectorAll('.key').forEach(function(key) {
+  keys.forEach(function(key) {
     key.addEventListener('click', function(){
       this.classList.toggle('key-select');
-      var i = parseInt(this.id);
+      var i = parseInt(this.dataset.index);
       userSequence[i].includes(2) ? userSequence[i].pop()
                                   : userSequence[i].push(2);
       console.log(userSequence);
     });
   });
 
+  // Play button event listener
+  var loopPlaying = false;
+  playButton.addEventListener('click', function() {
+    if (loopPlaying) return;
+    playSequence(playbackSequence);
+    loopPlaying = true;
+  });
 
+  // Stop button event listener
+  stopButton.addEventListener('click', function() {
+    if (!loopPlaying) return;
+    clearInterval(intervalID);
+    loopPlaying = false;
+  });
+
+  // Submit button event listener
+  submitButton.addEventListener('click', function() {
+    if (loopPlaying) return;
+    playSequence(userSequence);
+    loopPlaying = true;
+  })
+
+  // Tempo event listener
+  inputField.addEventListener('keypress', function(e) {
+    if (e.which === 13) {
+      if (inputField.value !== NaN) {
+        newTempo(inputField.value);
+        } else {
+          document.querySelector('#bad-input').style.display = 'initial'
+        }
+    }});
 
 
 });
